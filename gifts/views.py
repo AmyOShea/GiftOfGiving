d9fc3d9ce5fc8dbd049a8e0e3a09102419350148
neringa <neringa.bickmore@gmail.com>
2021-12-09 19:02:02 +0000

    cloudinary/initial setup

--- gifts/views.py
+++ gifts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 
 def gifts(request):
     """ View to render gifts template """
