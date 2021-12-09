eaeca6d560a049c6f3813f025dd5114c374ce84f
Daisy-McG <mcgirr.daisy@gmail.com>
2021-12-09 19:23:58 +0000

    Added profiles admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Profile, CharityAddress
+
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'name',
+        'organisation_name',
+        'type',
+        'verified'
+    )
+
+
+class CharityAddressAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'address_line_one',
+        'address_line_two',
+        'address_line_three',
+        'county',
+        'country',
+        'postcode'
+    )
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(CharityAddress, CharityAddressAdmin)
