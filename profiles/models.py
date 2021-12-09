46a1e723c8b0aebd7bd59101c8b9dde144360aaa
Daisy-McG <mcgirr.daisy@gmail.com>
2021-12-09 19:09:03 +0000

    Added profiles model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+USER_TYPE_CHOICES = (
+    ('charity', 'CHARITY'),
+    ('donor', 'DONOR')
+)
+
+
+class Profile(models.Model):
+    """
+    A model to create a profile for a user
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100,
+                             null=False, blank=False)
+    organisation_name = models.CharField(max_length=150,
+                            null=True, blank=True)
+    type = models.CharField(max_length=25,
+                                      choices=USER_TYPE_CHOICES,
+                                      null=False, blank=False)
+    verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+class CharityAddress(models.Model):
+    """ 
+    A model to create an address for a charity
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address_line_one = models.CharField(max_length=150,
+                            null=False, blank=False)
+    address_line_two = models.CharField(max_length=150,
+                            null=False, blank=False)
+    address_line_three = models.CharField(max_length=150,
+                            null=False, blank=False)
+    county = models.CharField(max_length=150,
+                            null=False, blank=False)
+    country = models.CharField(max_length=150,
+                            null=False, blank=False)
+    postcode = models.CharField(max_length=150,
+                            null=False, blank=False)
+
+    def __str__(self):
+        return self.organisation_name
