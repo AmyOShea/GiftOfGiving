5284dced7b38da2fee11cfb3c1a2f8f0b724ca24
Daisy-McG <mcgirr.daisy@gmail.com>
2021-12-10 01:24:44 +0000

    Fixed error with edit profile duplicating entries

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse
+from io import IOBase
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -22,37 +23,43 @@ def profile(request, user):
 
 def edit_profile(request, user):
     """ View to render edit profile and allow users to update profile """
-    user = request.user
+    global profile
+    try:
+        profile = get_object_or_404(Profile, user=request.user)
+    except:
+        Profile.objects.create(
+            user=request.user, name='',
+            organisation_name='', type='donor')
+
     if request.method == 'POST':
-        form = ProfileForm(request.POST)
+        form = ProfileForm(request.POST, instance=profile)
         if form.is_valid():
+            form = form.save(commit=False)
+            form.user = request.user
             form.save()
             messages.success(request, 'Profile information updated!')
-            return redirect(reverse('profile'))
+            return redirect(reverse('profile', args=[user]))
         else:
             messages.error(
                 request,
                 'ERROR: Profile update failed. Please try again.')
-
     else:
-        form = ProfileForm()
+        form = ProfileForm(instance=profile)
 
     context = {
         'form': form,
-        'user': user
     }
+    
     return render(request, 'profiles/edit_profile.html', context)
 
 
 def edit_address(request, user):
     """ View to render edit address and allow users to update address"""
-    user = request.user
     if request.method == 'POST':
         form = CharityAddressForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Charity address updated!')
-            return redirect(reverse('profile'))
         else:
             messages.error(
                 request,
