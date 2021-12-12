1c633c7aeb92f514518a49d6526dc6a75451002d
Daisy-McG <mcgirr.daisy@gmail.com>
2021-12-12 20:45:55 +0000

    Added missing semi colons to fix validator errors

--- static/js/base.js
+++ static/js/base.js
@@ -1,26 +1,26 @@
 let currentUrl = window.location.href;
 
 function createSnow() {
-    let snow = document.createElement('i');
+  let snow = document.createElement("i");
 
-    snow.classList.add('fas')
-    snow.classList.add('fa-circle')
+  snow.classList.add("fas");
+  snow.classList.add("fa-circle");
 
-    snow.style.left = Math.random() * window.innerWidth - 30 + 'px';
-    snow.style.animationDuration = Math.random()  *3 + 2 + 's';
-    snow.style.opacity = Math.random();
-    snow.style.fontSize = Math.random() + 'rem';
+  snow.style.left = Math.random() * window.innerWidth - 30 + "px";
+  snow.style.animationDuration = Math.random() * 3 + 2 + "s";
+  snow.style.opacity = Math.random();
+  snow.style.fontSize = Math.random() + "rem";
 
+  document.body.appendChild(snow);
 
-    document.body.appendChild(snow);
-
-    setTimeout(() => {
-        snow.remove()
-    }, 6000 )
+  setTimeout(() => {
+    snow.remove();
+  }, 6000);
 }
 
-if (currentUrl === 'https://8000-bronze-salamander-auu21iwj.ws-eu23.gitpod.io/' || currentUrl === 'https://gift-of-giving.herokuapp.com/' ) {
-    setInterval(createSnow, 10)
+if (
+  currentUrl === "https://8000-bronze-salamander-auu21iwj.ws-eu23.gitpod.io/" ||
+  currentUrl === "https://gift-of-giving.herokuapp.com/"
+) {
+  setInterval(createSnow, 10);
 }
-
-
